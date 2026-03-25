37e7388933c0bd3d5d87e3c2375d2705e78bf13c
Yanhuabcd820 <margaret0820@yahoo.com.tw>
2026-03-25 17:44:25 +0800

    slice header & banner

--- src/components/MainBanner.vue
+++ src/components/MainBanner.vue
@@ -1,6 +1,393 @@
-<script setup></script>
+<script setup>
+import { getImage } from "@/utils/getImage.js";
+</script>
 <template>
-  <h1>MainBanner</h1>
+  <div class="MainBanner">
+    <div class="container">
+      <div class="text-top">
+        <div class="text-1">
+          <span class="banner-H">
+            <img :src="getImage(`banner-H.svg`)" alt="" />
+          </span>
+          <span class="banner-U">
+            <svg viewBox="0 0 108 140">
+              <path
+                class="cls-1"
+                d="M69.26,2.9v93a15.26,15.26,0,1,1-30.52,0V2.9H3.32V92.48A44.75,44.75,0,0,0,47.94,137.1H60.06a44.75,44.75,0,0,0,44.62-44.62V2.9Z"
+              />
+            </svg>
+          </span>
+          <span class="banner-A-1">
+            <img :src="getImage(`banner-A-1.svg`)" alt="" />
+          </span>
+          <span class="banner-N-1">
+            <img :src="getImage(`banner-N-1.svg`)" alt="" />
+          </span>
+          <span class="banner-G banner-space">
+            <img :src="getImage(`banner-G.svg`)" alt="" />
+          </span>
+        </div>
+        <div class="text-2">
+          <span class="banner-Y">
+            <img :src="getImage(`banner-Y.svg`)" alt="" />
+          </span>
+          <span class="banner-A-3">
+            <svg viewBox="0 0 129 140">
+              <path
+                class="cls-1"
+                d="M85.1,4.05H46.69L2.65,136H36.71l9.5-28.45H83.4L92.29,136h34.06ZM51.85,82.41,65,31.08h1.49l10.6,51.33Z"
+              />
+            </svg>
+          </span>
+          <span class="banner-N banner-space">
+            <img :src="getImage(`banner-N-1.svg`)" alt="" />
+          </span>
+          <span class="banner-H">
+            <svg viewBox="0 0 105.32 140">
+              <polygon
+                class="cls-1"
+                points="66.99 4.05 66.99 54.6 38.33 54.6 38.33 4.05 3.73 4.05 3.73 135.95 38.33 135.95 38.33 82.34 66.99 82.34 66.99 135.95 101.59 135.95 101.59 4.05 66.99 4.05"
+              />
+            </svg>
+          </span>
+          <span class="banner-U">
+            <img :src="getImage(`banner-U.svg`)" alt="" />
+          </span>
+          <span class="banner-A-2">
+            <img :src="getImage(`banner-A-2.svg`)" alt="" />
+          </span>
+        </div>
+      </div>
+      <div class="text-middle">
+        <p>Everything in this world can be put down—except chopsticks.</p>
+      </div>
+      <div class="text-bottom">
+        <div class="text-3">
+          <span class="banner-P">
+            <img :src="getImage(`banner-P.svg`)" alt="" />
+          </span>
+          <span class="banner-O-1">
+            <img :src="getImage(`banner-O-1.svg`)" alt="" />
+          </span>
+          <span class="banner-R">
+            <img :src="getImage(`banner-R.svg`)" alt="" />
+          </span>
+          <span class="banner-T">
+            <svg viewBox="0 0 102.8 140">
+              <polygon
+                class="cls-1"
+                points="97.65 4.31 5.15 4.31 5.15 30.87 33.34 30.87 33.34 135.69 69.69 135.69 69.69 30.87 97.65 30.87 97.65 4.31"
+              />
+            </svg>
+          </span>
+        </div>
+        <div class="text-4">
+          <span class="banner-F">
+            <img :src="getImage(`banner-F.svg`)" alt="" />
+          </span>
+          <span class="banner-O-2">
+            <img :src="getImage(`banner-O-2.svg`)" alt="" />
+          </span>
+          <span class="banner-L">
+            <img :src="getImage(`banner-L.svg`)" alt="" />
+          </span>
+          <span class="banner-I">
+            <img :src="getImage(`banner-I.svg`)" alt="" />
+          </span>
+          <span class="banner-O-3 banner-space">
+            <img :src="getImage(`banner-O-3.svg`)" alt="" />
+          </span>
+        </div>
+        <div class="text-5">
+          <span class="banner-2-1">
+            <svg viewBox="0 0 96.19 140">
+              <path
+                class="cls-1"
+                d="M8.08,11.06,9.82,40.42s21.67-19.23,39.5-6.3-15,53.13-43,76.9v26.07H89.86V111.72H42.68S78.33,81.31,87.42,50.2,69.94-12.36,8.08,11.06Z"
+              />
+            </svg>
+          </span>
+          <span class="banner-0">
+            <svg viewBox="0 0 96.19 140">
+              <path
+                class="cls-1"
+                d="M93.69,70c0-11.68-.75-22.67-3.39-32.28C85.64,17.84,69.43,1.89,48.44,1.89c-19.86,0-32.36,9.54-40.53,32.61,0,0-5.41,13.87-5.41,35.5,0,16,2.82,30.7,7.52,42.33h0a41.5,41.5,0,0,0,38.4,25.78c21,0,37.55-16,42.06-35.8C93.69,87.4,93.69,81.69,93.69,70Z"
+              />
+              <ellipse
+                class="cls-1"
+                cx="48.69"
+                cy="70.27"
+                rx="12.95"
+                ry="44.16"
+              />
+            </svg>
+          </span>
+          <span class="banner-2-2">
+            <svg viewBox="0 0 96.19 140">
+              <path
+                class="cls-1"
+                d="M8.08,11.06,9.82,40.42s21.67-19.23,39.5-6.3-15,53.13-43,76.9v26.07H89.86V111.72H42.68S78.33,81.31,87.42,50.2,69.94-12.36,8.08,11.06Z"
+              />
+            </svg>
+          </span>
+          <span class="banner-6">
+            <svg viewBox="0 0 96.19 140">
+              <ellipse
+                class="cls-1"
+                cx="48.86"
+                cy="95.3"
+                rx="11.53"
+                ry="20.62"
+              />
+              <path
+                class="cls-1"
+                d="M85.33,7,83.47,32.82S39.66,6.49,35.47,61a25.86,25.86,0,0,1,23.3-10.48c15.84,1.16,36.11,13.75,33.08,44.85S75.78,138,47.35,138.05,6.81,113.44,4.71,90.37,4.48,29.56,23.82,13,75.31,2.06,85.33,7Z"
+              />
+            </svg>
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.MainBanner {
+  overflow: hidden;
+  background-color: var(--blue);
+  padding: 10% 0 13%;
+  .container {
+    width: 100%;
+    max-width: 1536px;
+    margin: 0 auto;
+  }
+  .text-top,
+  .text-bottom {
+    display: flex;
+    justify-content: center;
+  }
+  img {
+    height: 100%;
+    width: auto;
+  }
+  [class^="banner-"] {
+    svg {
+      transform: scale(0.96);
+      height: 100%;
+    }
+  }
+  .text-1,
+  .text-2,
+  .text-3,
+  .text-4,
+  .text-5 {
+    display: flex;
+  }
+
+  .text-1 {
+    width: 43.5%;
+    [class^="banner-"] {
+      padding-right: 2%;
+    }
+    .banner-H {
+      width: 14.64%;
+    }
+    .banner-U {
+      width: 15.69%;
+    }
+    .banner-A-1 {
+      width: 19.28%;
+      margin-left: -1.9%;
+    }
+    .banner-N-1 {
+      width: 16.74%;
+    }
+    .banner-G {
+      width: 17.48%;
+    }
+  }
+  .text-2 {
+    width: 50.39%;
+    [class^="banner-"] {
+      padding-right: 2%;
+      &.banner-space {
+        padding-right: 7.75%;
+      }
+    }
+    .banner-Y {
+      width: 11.5%;
+    }
+    .banner-A-3 {
+      width: 16.66%;
+      margin-left: -1%;
+    }
+    .banner-N {
+      width: 14.47%;
+    }
+    .banner-H {
+      width: 12.66%;
+    }
+    .banner-U {
+      width: 13.56%;
+    }
+    .banner-A-2 {
+      width: 16.66%;
+      margin-left: -2.3%;
+      position: relative;
+      z-index: 0;
+      &::after {
+        position: absolute;
+        top: -60%;
+        left: 0;
+        content: "";
+        background-image: url("../assets/images/banner-deco1.png");
+        background-size: cover;
+        width: 275%;
+        aspect-ratio: 330 / 285;
+        background-repeat: no-repeat;
+        z-index: -1;
+      }
+    }
+  }
+  .text-3 {
+    width: 29.68%;
+    [class^="banner-"] {
+      padding-right: 2%;
+    }
+    .banner-P {
+      width: 21.05%;
+      position: relative;
+      z-index: 0;
+      &::after {
+        position: absolute;
+        top: 5%;
+        right: 5%;
+        content: "";
+        background-image: url("../assets/images/banner-deco2.png");
+        background-size: cover;
+        width: 378%;
+        aspect-ratio: 344 / 312;
+        background-repeat: no-repeat;
+        z-index: -1;
+      }
+    }
+    .banner-O-1 {
+      width: 26.31%;
+    }
+    .banner-R {
+      width: 22.36%;
+    }
+    .banner-T {
+      width: 22.36%;
+      .cls-1 {
+        stroke: #ff82ff;
+      }
+    }
+  }
+  .text-4 {
+    width: 36.58%;
+    [class^="banner-"] {
+      padding-right: 2%;
+      &.banner-space {
+        padding-right: 1.67%;
+      }
+    }
+    .banner-F {
+      width: 14.76%;
+    }
+    .banner-O-2,
+    .banner-O-3 {
+      width: 21.35%;
+    }
+    .banner-L {
+      width: 14.94%;
+    }
+    .banner-I {
+      width: 7.11%;
+    }
+  }
+  .text-5 {
+    width: 24.73%;
+    .banner-2-1,
+    .banner-2-2,
+    .banner-0,
+    .banner-6 {
+      width: 25.26%;
+    }
+    .banner-2-1,
+    .banner-6 {
+      .cls-1 {
+        stroke: #e7ff49;
+      }
+    }
+    .banner-0 {
+      .cls-1 {
+        stroke: #ffffff;
+      }
+    }
+    .banner-2-2 {
+      .cls-1 {
+        stroke: #ff82ff;
+      }
+    }
+  }
+
+  .cls-1 {
+    fill: none;
+    stroke: #fff;
+    stroke-miterlimit: 10;
+    stroke-width: 4px;
+  }
+  .text-middle {
+    p {
+      font-family: "Inter", sans-serif;
+      color: var(--white);
+      font-size: clamp(20px, 4vw, 36px);
+      text-align: center;
+      padding: 3% 0;
+      line-height: 125%;
+    }
+  }
+}
+@media screen and (max-width: 768px) {
+  .MainBanner {
+    .text-top,
+    .text-bottom {
+      flex-direction: column;
+      flex-wrap: wrap;
+      align-items: center;
+    }
+    .text-1 {
+      width: 69.1%;
+      justify-content: center;
+      padding-bottom: 3%;
+    }
+    .text-2 {
+      width: 83.3%;
+      .banner-A-2 {
+        &::after {
+          top: -100%;
+        }
+      }
+    }
+    .text-3 {
+      width: 49.1%;
+      padding-bottom: 3%;
+      .banner-P {
+        &::after {
+          right: 45%;
+        }
+      }
+    }
+    .text-4 {
+      width: 56.3%;
+      z-index: 0;
+      justify-content: center;
+      padding-bottom: 3%;
+    }
+    .text-5 {
+      width: 43.3%;
+    }
+  }
+}
+</style>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,8 +1,231 @@
-<script setup></script>
+<script setup>
+import { getImage } from "@/utils/getImage.js";
+</script>
 <template>
   <header>
-    <h1>header</h1>
+    <h1>
+      <div class="logo">
+        <img :src="getImage(`logo.svg`)" alt="Huang Yan Hua portfolio 2026" />
+      </div>
+    </h1>
+    <div class="marquee">
+      <p>
+        Be bold, be brave, be you. Stay hungry, stay foolish. In this world, the
+        only things I can't put down are my chopsticks. In this world, the only
+        things I can't put down are my chopsticks. Be bold, be brave, be you.
+        Stay hungry, stay foolish. In this world, the only things I can't put
+        down are my chopsticks. In this world, the only things I can't put down
+        are my chopsticks.
+      </p>
+    </div>
+    <button class="menuCheck">
+      <div class="bar-group">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+      </div>
+    </button>
+    <nav>
+      <ul>
+        <li><a href="#about">About</a> <span>/</span></li>
+        <li>
+          <a href="#experience">Experience</a>
+          <span>/</span>
+        </li>
+        <li>
+          <a href="#works">Works</a>
+          <span>/</span>
+        </li>
+        <li>
+          <a href="#resume">Resume</a>
+          <span>/</span>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+header {
+  color: var(--white);
+  background-color: var(--blue);
+  display: flex;
+  align-items: flex-start;
+  h1 {
+    padding: 24px 44px;
+    border-right: 4px solid var(--white);
+    border-bottom: 4px solid var(--white);
+    .logo {
+      width: 92px;
+    }
+  }
+  .marquee {
+    padding: 24px 0;
+    border-right: 4px solid var(--white);
+    border-bottom: 4px solid var(--white);
+    flex-grow: 1;
+    overflow: hidden;
+    p {
+      white-space: nowrap;
+      animation: marquee 10s linear infinite;
+      letter-spacing: 3px;
+    }
+  }
+  button.menuCheck {
+    display: none;
+    width: 50px;
+    height: 50px;
+    flex-shrink: 0;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 2px solid var(--white);
+    .bar-group {
+      position: relative;
+      width: 24px;
+      height: 24px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .bar {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 24px;
+      height: 3px;
+      background-color: var(--white);
+      transition: 0.5s;
+
+      &:nth-of-type(2) {
+        top: 50%;
+        transform: translateY(-50%);
+      }
+
+      &:nth-of-type(3) {
+        top: initial;
+        bottom: 0;
+      }
+    }
+    &.isMenuOpen {
+      .bar {
+        &:nth-of-type(1) {
+          top: 50%;
+          transform: translateY(-50%) rotate(-45deg);
+        }
+
+        &:nth-of-type(2) {
+          top: 50%;
+          transform: translateY(-50%) rotate(45deg);
+        }
+
+        &:nth-of-type(3) {
+          display: none;
+        }
+      }
+    }
+  }
+  nav {
+    padding: 24px 20px;
+    border-bottom: 4px solid var(--white);
+
+    ul {
+      display: flex;
+      li {
+        display: flex;
+        a {
+          color: var(--white);
+          padding: 0 24px;
+        }
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  header {
+    h1 {
+      padding: 12px 16px;
+      border-right: 2px solid var(--white);
+      border-bottom: 2px solid var(--white);
+      .logo {
+        width: 56px;
+      }
+    }
+    .marquee {
+      padding: 12px;
+      border-right: 2px solid var(--white);
+      border-bottom: 2px solid var(--white);
+    }
+    nav {
+      padding: 12px 16px;
+      border-bottom: 2px solid var(--white);
+
+      ul {
+        li {
+          a {
+            padding: 0 12px;
+          }
+        }
+      }
+    }
+  }
+}
+@media screen and (max-width: 768px) {
+  header {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    &.isMenuOpen {
+      height: 100vh;
+      position: absolute;
+      z-index: 20;
+    }
+    .marquee {
+      width: 40%;
+    }
+    button.menuCheck {
+      display: flex;
+    }
+    nav {
+      display: none;
+      width: 100%;
+      height: calc(100% - 64px);
+      padding: 0px;
+      border-bottom: none;
+      ul {
+        justify-content: center;
+        flex-direction: column;
+        width: 100%;
+        gap: 8px;
+
+        li {
+          justify-content: center;
+          flex-direction: column;
+          gap: 8px;
+          a {
+            display: block;
+            text-align: center;
+          }
+          span {
+            margin: 0 auto;
+          }
+        }
+      }
+      &.isMenuOpen {
+        display: flex;
+      }
+    }
+  }
+}
+@keyframes marquee {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-50%);
+  }
+}
+</style>
